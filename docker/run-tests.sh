c655fe4d301f3be46ff222b985c1ce7379b86e10
VladimirKBG <owl2666@mail.ru>
2025-10-01 22:13:04 +0500

    bugfix

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -13,19 +13,4 @@ until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
 done
 echo "Postgres is ready."
 
-echo "Waiting for web at $WEB_URL ..."
-i=0
-while true; do
-  if curl -fs "$WEB_URL" >/dev/null 2>&1; then
-    echo "Web is ready."
-    break
-  fi
-  i=$((i+1))
-  if [ "$i" -gt 60 ]; then
-    echo "Timeout waiting for web"
-    exit 1
-  fi
-  sleep 1
-done
-
 exec "$@"
